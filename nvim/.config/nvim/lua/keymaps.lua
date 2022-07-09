020e59d5760575c4bcb78fabbbabda3f7096e49c
Josh Vocal <joshvocal@gmail.com>
2022-07-08 20:05:22 -0700

    Get telescope keymaps working

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,5 +1,10 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true }
 
+vim.g.mapleader = ','
+
 -- Press jj to ESC
 map('i', 'jj', '<Esc>', opts)
+
+-- Reload configuration without restark nvim
+map('n', '<leader>r', ':so %<CR>', opts)
