529e6344ea180ba66752e54ad49674019b2c20f9
Josh Vocal <joshvocal@gmail.com>
2022-07-08 19:11:50 -0700

    Formatting

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -7,5 +7,11 @@ return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  use { 'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} }}
+  use {
+      'nvim-telescope/telescope.nvim',
+      requires = {
+          { 'nvim-lua/plenary.nvim' }
+      }
+  }
+
 end)
