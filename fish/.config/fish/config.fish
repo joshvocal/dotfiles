e59f2236cfd3f67aff140065c2a7c76787b42228
Josh Vocal <joshvocal@gmail.com>
2023-12-27 10:29:39 -0800

    Create config.fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Terminal
+alias ls='ls -G'
+alias ll='ls -l -G'
+alias lla='ls -la -G'
+alias h='history'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias gs='g s'
+
+# Dotfiles
+alias brewup="brew update && brew upgrade && brew cleanup"
+alias stmux='tmux source-file ~/.tmux.conf'
+
+# Git
+alias g="git"
