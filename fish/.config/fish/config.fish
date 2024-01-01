4b7bd16f1b9b94c191dc7a326005ba4a234227ed
Josh Vocal <joshvocal@gmail.com>
2024-01-01 14:12:19 -0800

    Updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -17,3 +17,8 @@ alias tmuxup='tmux source-file ~/.tmux.conf'
 
 # Git
 alias g="git"
+
+# Created by `pipx` on 2023-12-28 16:20:50
+#set PATH $PATH /Users/joshvocal/.local/bin
+#set -x GOPATH /Users/joshvocal/go
+#set -x GO111MODULE on
